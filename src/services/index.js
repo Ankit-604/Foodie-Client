b9d272a450feea58608add538869e51be90257df
Ankit Kumar Sharma <75676084+Ankit-604@users.noreply.github.com>
2024-12-02 11:02:10 +0530

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = "https://foodie-server-v291.onrender.com";
+
 console.log("BACKEND_URL:", BACKEND_URL);
 export const userSignUp = async (data) => {
   try {
